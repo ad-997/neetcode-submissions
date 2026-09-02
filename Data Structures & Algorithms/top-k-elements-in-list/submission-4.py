7491a941c7892cc51f96129c12a7c03067002e35
aditya-borkar <91491479+ad-997@users.noreply.github.com>
2026-09-02 21:44:39 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,5 @@
+from collections import OrderedDict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), count.get)
